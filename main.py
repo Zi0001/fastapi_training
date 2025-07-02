2acd3374d9c5cbae0eabd6c0e503ec285cafdcd1
Zi0001 <WorckAcTop.218@gmail.com>
2025-07-02 21:54:58 +0300

    test2

--- main.py
+++ main.py
@@ -24,44 +24,42 @@ def hello_author():
     return {'Hello': 'author'}
 
 @app.get(
-        '/{name}',
-        tags=['common methods'],
-        summary='Общее приветствие',
-        response_description='Полная строка приветствия'
+    '/{name}',
+    tags=['common methods'],
+    summary='Общее приветствие',
+    response_description='Полная строка приветствия'
 )
 def greetings(
         *,
-        name: str = Path(..., min_length=2, max_length=20),
+        name: str = Path(
+            ..., min_length=2, max_length=20,
+            title='Полное имя', description='Можно вводить в любом регистре'
+        ),
+        surname: list[str] = Query(..., min_length=2, max_length=50),
         age: Optional[int] = Query(None, gt=4, le=99),
-        is_staff: bool = False,
-        education_level: Optional[EducationLevel] = None,
-        surname: str = Query(..., min_length=2, max_length=50),
+        is_staff: bool = Query(
+            False, alias='is-staff', include_in_schema=False
+        ),
+        education_level: Optional[EducationLevel] = None, 
 ) -> dict[str, str]:
     """
     Приветствие пользователя:
 
     - **name**: имя
-    - **surname**: фамилия
+    - **surname**: фамилия или несколько фамилий
     - **age**: возраст (опционально)
-    - **is_staff**: является ли пользователь сотрудником
-    - **education_level**: уровень образования (опционально)
+    - **title**: обращение
     """
-    result = ' '.join([name, surname])
-
-    result = result.title()
-
+    surnames = ' '.join(surname)
+    result = ' '.join([name, surnames])
+    result = result.title()    
     if age is not None:
         result += ', ' + str(age)
-
     if education_level is not None:
-        # Чтобы текст смотрелся грамотно, 
-        # переведём строку education_level в нижний регистр.
         result += ', ' + education_level.lower()
-
     if is_staff:
         result += ', сотрудник'
-
-    return {'Hello': result} 
+    return {'Hello': result}
 
 
 if __name__ == '__main__':
