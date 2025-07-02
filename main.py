b3038b17ae167c7a2e2f2d3e0b224649f81f6d13
Zi0001 <WorckAcTop.218@gmail.com>
2025-07-02 05:43:41 +0300

    test

--- main.py
+++ main.py
@@ -1,39 +1,69 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 import uvicorn
 
 from typing import Optional
 
+from enum import Enum
+
 # Создание объекта приложения.
 app = FastAPI()
 # app = FastAPI(docs_url=None, redoc_url=None) 
 
-# @app.get('/')
-# def read_root():
-#     return {'Hello': 'FastAPI'}
 
-@app.get('/me')
+
+class EducationLevel(str, Enum):
+    # Укажем значения с большой буквы, чтобы они хорошо смотрелись 
+    # в документации Swagger.
+    SECONDARY = 'Среднее образование'
+    SPECIAL = 'Среднее специальное образование'
+    HIGHER = 'Высшее образование'
+
+
+@app.get('/me', tags=['special methods'], summary='Приветствие автора')
 def hello_author():
     return {'Hello': 'author'}
 
-@app.get('/{name}')
+@app.get(
+        '/{name}',
+        tags=['common methods'],
+        summary='Общее приветствие',
+        response_description='Полная строка приветствия'
+)
 def greetings(
-        name: str,
-        surname: str,
+        *,
+        name: str = Path(..., min_length=2, max_length=20),
         age: Optional[int] = None,
-        is_staff: bool = False
+        is_staff: bool = False,
+        education_level: Optional[EducationLevel] = None,
+        surname: str = Query(..., min_length=2, max_length=50),
 ) -> dict[str, str]:
+    """
+    Приветствие пользователя:
+
+    - **name**: имя
+    - **surname**: фамилия
+    - **age**: возраст (опционально)
+    - **is_staff**: является ли пользователь сотрудником
+    - **education_level**: уровень образования (опционально)
+    """
     result = ' '.join([name, surname])
+
     result = result.title()
+
     if age is not None:
         result += ', ' + str(age)
+
+    if education_level is not None:
+        # Чтобы текст смотрелся грамотно, 
+        # переведём строку education_level в нижний регистр.
+        result += ', ' + education_level.lower()
+
     if is_staff:
         result += ', сотрудник'
+
     return {'Hello': result} 
 
 
 if __name__ == '__main__':
-    # Команда на запуск uvicorn.
-    # Здесь же можно указать хост и/или порт при необходимости,
-    # а также другие параметры.
     uvicorn.run('main:app', reload=True) 
